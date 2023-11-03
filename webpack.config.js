2d90075524c17cc68cb7557225b1a08d53f59a9f
Sqwatermelon01 <149752274+Sqwatermelon01@users.noreply.github.com>
2023-11-03 13:19:39 -0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  entry: './public/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  
+},
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+}
